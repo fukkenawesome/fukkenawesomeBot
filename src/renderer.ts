517152acc207fc972d4f732c22845487ad20fa86
fukkenawesome <mrbigballs@mrbigballs.net>
2017-11-26 09:05:34 +0100

    imported tmi.js

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,24 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var tmi = require("tmi.js");
+//import * as tmi2 from 'tmi.js';
+
+var options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        reconnect: true
+    },
+    identity: {
+        username: "fukkenawesomeBot",
+        password: "oauth:ppjnhfg4obg377xghlujfw5pwull12"
+    },
+    channels: ["#fukkenawesome"]
+};
+
+var client = new tmi.client(options);
+
+// Connect the client to the server..
+client.connect();
